4c6e3912feb7e45cdfd90f692b31e5175968d944
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-31 20:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 function greet(timestr) {}
-  const hour = parseInt(timestr, 10); 
-  
+  const hour = parseInt(timestr, 10);
+
   if  (hour < 12 ) return "Good Morning"
   if (hour < 17) return "Good Evening"
   return "Good Afternoon"
-}   
-   
+}
+
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
