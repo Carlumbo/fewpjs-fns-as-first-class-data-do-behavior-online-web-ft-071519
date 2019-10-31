76885fb8b758489e37ed9d66f1147bf1e4ece628
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-31 20:53:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function handleClick(e) {
 function greet(timeString) {}
   const hour = parseInt(timeString, 10);
 
-  if  ( hour < 12 ) return "Good Morning"
+  if ( hour < 12) return "Good Morning"
   if ( hour < 17) return "Good Evening"
   return "Good Afternoon"
 }
